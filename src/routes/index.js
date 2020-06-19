1e05f803d12d155d1f40d60ed9d2d2530e129470
hiroki hokari <hiro.hokari.0722@gmail.com>
2020-06-19 20:11:37 +0900

    Add api for creating new message

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { indexPage, messagesPage } from '../controllers'
+import { indexPage, messagesPage, addMessage } from '../controllers'
 import { testEnvironmentVariable } from '../config'
 
 const indexRouter = express.Router()
 
 indexRouter.get('/', indexPage)
 indexRouter.get('/messages', messagesPage)
+indexRouter.post('/messages', addMessage)
 
 export default indexRouter
